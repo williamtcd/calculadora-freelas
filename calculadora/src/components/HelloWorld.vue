a00a7648741fa95783b38758f8a86084d2580f4a
William Tavares <William@editora.pec.br>
2020-03-16 14:19:23 -0300

    WIP - inclusao de despesas

--- calculadora/src/components/HelloWorld.vue
+++ calculadora/src/components/HelloWorld.vue
@@ -43,10 +43,10 @@
             ></v-text-field>
             <v-text-field
               label="Total de despesas por mês ?"
-              v-model="despesas"
+              v-model="totalDespesas"
               @input="calculo"
               outlined
-              dense            
+              dense
               prefix="R$"              
               type="number"
               :min="0"
@@ -59,6 +59,28 @@
               type="number"
               readonly
             ></v-text-field>
+            <ul>
+            <li v-for="despesa in despesas" :key="despesa.id">
+            {{ despesa.nome }}
+            {{ despesa.valor }}</li>
+          </ul>
+          <v-form>
+          <v-text-field
+              label="nome"
+              v-model="form.dNome"
+              outlined
+              dense
+            ></v-text-field>
+            <v-text-field
+              label="valor"
+              v-model="form.dValor"
+              outlined
+              dense
+              prefix="R$"
+              type="number"
+            ></v-text-field>
+            <v-btn @click="incluir(form)" small color="primary">Salva</v-btn>
+            </v-form>
       </v-col>
     </v-row>
   </v-container>
@@ -76,7 +98,7 @@ import { mask } from 'vue-the-mask'
       horasDia: '',
       diasSemana: '',
       semanaFerias: '',
-      despesas: '',
+      totalDespesas: '',
       horaTecnica: '',
       horasAno: '',
       lucroAno: '',
@@ -84,7 +106,18 @@ import { mask } from 'vue-the-mask'
       jobHoras: '',
       jobTotal: '',
       min: '',
+      form: {        
+        dNome: '',
+        dValor: ''
+      },
+      despesas: [
+        { nome: 'Aluguel', valor: '800' },
+        { nome: 'luz', valor: '15.5' },
+      ]
     }),
+    created () {
+      this.contador()
+    },
     methods: {
       calculo: function() {          
           if(this.semanaFerias){
@@ -92,13 +125,34 @@ import { mask } from 'vue-the-mask'
           }else{
             this.horasAno = this.horasDia*this.diasSemana*(48)
           }
-          if(this.despesas){              
-          this.lucroAno = (parseFloat(this.salarioLiquido) + parseFloat(this.despesas))*12
+          if(this.totalDespesas){              
+          this.lucroAno = (parseFloat(this.salarioLiquido) + parseFloat(this.totalDespesas))*12
           }else{
             this.lucroAno = parseFloat(this.salarioLiquido)*12
           }          
           let calcula = (this.lucroAno/100*110)/this.horasAno
           this.horaTecnica = calcula.toFixed(2)
+      },
+      contador () {
+        this.totalDespesas = 0
+        const desp = this.despesas
+        desp.forEach((despesa) => {
+          console.log(despesa.valor)          
+          if(!this.totalDespesas){
+            this.totalDespesas = parseFloat(despesa.valor)
+          } else {
+          this.totalDespesas = parseFloat(this.totalDespesas)+parseFloat(despesa.valor)
+          }
+          console.log(this.totalDespesas)
+        })
+      },
+      incluir (desp) {
+        console.log(desp.dNome + ' ' + desp.dValor)
+        this.despesas.push({nome: desp.dNome, valor: desp.dValor})
+        this.form.dNome = ''
+        this.form.dValor = ''
+        this.contador()
+        this.calculo()
       },
       verificaFerias () {
         if(this.semanaFerias > 47){
